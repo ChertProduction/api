cf331672f2e67a7d31059595e00a87e2d29f8fb8
kykara4uk <ivanserdechenkosergeevich@gmail.com>
2021-07-21 17:15:51 +0300

    1
    1

--- api/settings.py
+++ api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-^o)9h10iy%eh&xj8ke6f9%%*#r+ftk&a#^uhra48#jm-u8^sbf'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
